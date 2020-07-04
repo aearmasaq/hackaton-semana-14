be9e2b9090abdfe864d3b46108f009bc43196a7c
aearmasaq <63509650+aearmasaq@users.noreply.github.com>
2020-07-04 09:01:57 -0500

    Se agrega reto 1 de la semana 14

---
+++ js/reto-1.js
@@ -0,0 +1,14 @@
+var reto1 = new Vue({
+    el: '#app',
+    data: {
+      rtpta1: `El usar un framework nos ayudan a tener un mejor codigo 
+      en nuestro entorno de trabajo al momento del desarrollo de un sistema 
+      o web ya que nos proveen de una estructura y patrones a seguir para    
+        poder resolver problemas, como en el caso de Vue.JS nos trae directivas, methodos, etc.`,
+      rtpta2: `Los framework nos ayudan a tener un código ordenado y 
+      aceleran el avance de un proyecto ya que nos dan funcionalidades ya hechas y con buena practica.`,
+      rtpta3: `El no usar un framework nos limita mucho al momento del desarrollo de un sistema o web 
+      ya que tardaríamos mucho en implementar funcionalidades que por defecto ya traen los framework,
+       ademas de tener un código espagueti y mucho código.`
+    }
+  });
